92be58e96ec6986cc36654c53460d44306e93f08
hayleyskyland <hwitherell@skylandanalytics.net>
2021-12-23 14:12:48 -0700

    Add random histogram

--- main.js
+++ main.js
@@ -1,11 +1,29 @@
-TESTER = document.getElementById('tester');
+// tester
 
-Plotly.plot( TESTER,
-  [{
-    x: [1, 2, 3, 4, 5],
-    y: [1, 2, 4, 8, 16]
-  }],
-  { margin: { t: 0 } },
-  {showSendToCloud:true}
-);
-    
+// TESTER = document.getElementById('tester');
+
+// Plotly.plot( TESTER,
+//   [{
+//     x: [1, 2, 3, 4, 5],
+//     y: [1, 2, 4, 8, 16]
+//   }],
+//   { margin: { t: 0 } },
+//   {showSendToCloud:true}
+// );
+
+// histogram
+
+var x = [];
+
+for (var i = 0; i < 500; i ++) {
+	x[i] = Math.random();
+}
+
+var trace = {
+  x: x,
+  type: 'histogram',
+};
+
+var data = [trace];
+
+Plotly.newPlot('histogram', data);
