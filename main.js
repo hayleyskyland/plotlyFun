c40187313f8044477bf42c8b75bf97be0df271be
hayleyskyland <hwitherell@skylandanalytics.net>
2021-12-23 15:47:14 -0700

    Adjust average

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ const histogram = () => {
       name: 'Current',
       x: xCurrent,
       type: 'histogram',
-      marker: { color: 'lightgray' },
+      marker: { color: 'lightblue' },
     }
   ]
 
@@ -41,43 +41,43 @@ const histogram = () => {
 // scatter
 
 const scatter = () => {
-  const x1 = [1, 2, 3, 4, 5];
-  const x2 = [2, 2, 5, 4, 6];
-  const x3 = [1, 1, 2, 8, 8];
+  const y1 = [1, 2, 3, 4, 5];
+  const y2 = [2, 2, 5, 4, 6];
+  const y3 = [1, 1, 2, 8, 8];
 
   const avg = (i) => {
-    const sum = x1[i] + x2[i] + x3[i];
+    const sum = y1[i] + y2[i] + y3[i];
     return sum / 3;
   }
 
-  const xAvg = [avg(0), avg(1), avg(2), avg(3), avg(4)]
+  const yAvg = [avg(0), avg(1), avg(2), avg(3), avg(4)]
 
   const trace = [
     {
       name: '1st Run',
       mode: 'markers',
-      x: x1,
+      y: y1,
       type: 'scatter',
       marker: { color: 'pink' },
     },
     {
       name: '2nd Run',
       mode: 'markers',
-      x: x2,
+      y: y2,
       type: 'scatter',
       marker: { color: 'plum' },
     },
     {
       name: '3rd Run',
       mode: 'markers',
-      x: x3,
+      y: y3,
       type: 'scatter',
       marker: { color: 'lightblue' },
     },
     {
       name: 'Average',
-      mode: 'line',
-      x: xAvg,
+      mode: 'lines',
+      y: yAvg,
       type: 'scatter',
       marker: { color: 'lightgray' },
     }
