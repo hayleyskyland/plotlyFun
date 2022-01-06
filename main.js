7b73cc0bc4122cf0abe87cf6306423da50007d98
hayleyskyland <hwitherell@skylandanalytics.net>
2022-01-06 12:55:20 -0700

    Re-add top 2 charts

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-// import { histogram } from './histogram.js';
-// import { scatter } from './scatter.js';
+import { histogram } from './histogram.js';
+import { scatter } from './scatter.js';
 // import { d3MatrixTest } from './d3MatrixTest.js';
 // import { d3Matrix } from './d3Matrix.js';
 // import { plotMatrixTest } from './plotMatrixTest.js';
 import { plotMatrix } from './plotMatrix.js';
 
-// histogram()
-// scatter()
+histogram()
+scatter()
 // d3MatrixTest()
 // d3Matrix()
 // plotMatrixTest()
