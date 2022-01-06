5653ac936e7fbfcd4ea054664e67f6f08a3b52d8
hayleyskyland <hwitherell@skylandanalytics.net>
2022-01-06 10:30:53 -0700

    Start plotly matrix

--- main.js
+++ main.js
@@ -2,10 +2,10 @@
 // import { scatter } from './scatter.js';
 // import { d3MatrixTest } from './d3MatrixTest.js';
 // import { d3Matrix } from './d3Matrix.js';
-// import { plotMatrixTest } from './plotMatrixTest.js';
+import { plotMatrixTest } from './plotMatrixTest.js';
 
 // histogram()
 // scatter()
 // d3MatrixTest()
 // d3Matrix()
-// plotMatrixTest()
+plotMatrixTest()
