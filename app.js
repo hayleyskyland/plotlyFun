f30a12e5f2ef89a0275a520ba5667ccd94c1e976
hayleyskyland <hwitherell@skylandanalytics.net>
2022-01-07 12:14:44 -0700

    Organize files

---
+++ app.js
@@ -0,0 +1,13 @@
+// import { histogram } from './charts/tests/histogram.js';
+// import { scatter } from './charts/tests/scatter.js';
+// import { d3MatrixTest } from './charts/tests/d3-matrix/d3-test/d3MatrixTest.js';
+// import { d3Matrix } from './charts/tests/d3-matrix/final/d3Matrix.js';
+// import { plotMatrixTest } from './charts/tests/plotMatrixTest.js';
+import { plotMatrix } from './charts/plot-matrix/plotMatrix.js';
+
+// histogram()
+// scatter()
+// d3MatrixTest()
+// d3Matrix()
+// plotMatrixTest()
+plotMatrix()
---
+++ charts/plot-matrix/plotMatrixTraces.js
@@ -0,0 +1,234 @@
+// row A
+
+export var traceA1 = {
+  x: [1, 2, 3],
+  y: [4, 5, 6],
+  xaxis: 'x1',
+  yaxis: 'y1',    
+  type: 'histogram',
+  marker: { color: 'plum' },
+};
+
+export var traceA2 = {
+  x: [1, 2, 3],
+  y: [4, 5, 6],
+  xaxis: 'x2',
+  yaxis: 'y2',    
+  type: 'scatter',
+  marker: { color: 'lightblue' },
+};
+
+export var traceA3 = {
+  x: [20, 30, 40],
+  y: [50, 60, 70],
+  xaxis: 'x3',
+  yaxis: 'y3',
+  type: 'scatter',
+  marker: { color: 'lightgray' },
+};
+
+export var traceA4 = {
+  x: [20, 30, 40],
+  y: [50, 60, 70],
+  xaxis: 'x4',
+  yaxis: 'y4',
+  type: 'scatter',
+  marker: { color: '#F5DEB3' },
+};
+
+export var traceA5 = {
+  x: [20, 30, 40],
+  y: [50, 60, 70],
+  xaxis: 'x5',
+  yaxis: 'y5',
+  type: 'scatter',
+  marker: { color: 'pink' },
+};  
+
+// row B
+
+export var traceB1 = {
+  x: [300, 400, 500],
+  y: [600, 700, 800],
+  xaxis: 'x6',
+  yaxis: 'y6',
+  type: 'scatter',
+  marker: { color: 'lightblue' },
+};
+
+export var traceB2 = {
+  x: [4000, 5000, 6000],
+  y: [7000, 8000, 9000],
+  xaxis: 'x7',
+  yaxis: 'y7',
+  type: 'histogram',
+  marker: { color: 'lightgray' },
+};
+
+export var traceB3 = {
+  x: [4000, 5000, 6000],
+  y: [7000, 8000, 9000],
+  xaxis: 'x8',
+  yaxis: 'y8',
+  type: 'scatter',
+  marker: { color: '#F5DEB3' },
+};
+
+export var traceB4 = {
+  x: [4000, 5000, 6000],
+  y: [7000, 8000, 9000],
+  xaxis: 'x9',
+  yaxis: 'y9',
+  type: 'scatter',
+  marker: { color: 'pink' },
+};
+
+export var traceB5 = {
+  x: [4000, 5000, 6000],
+  y: [7000, 8000, 9000],
+  xaxis: 'x10',
+  yaxis: 'y10',
+  type: 'scatter',
+  marker: { color: 'plum' },
+};
+
+// row C
+
+export var traceC1 = {
+  x: [1, 2, 3],
+  y: [4, 5, 6],
+  xaxis: 'x11',
+  yaxis: 'y11',    
+  type: 'scatter',
+  marker: { color: 'lightgray' },
+};
+
+export var traceC2 = {
+  x: [20, 30, 40],
+  y: [50, 60, 70],
+  xaxis: 'x12',
+  yaxis: 'y12',
+  type: 'scatter',
+  marker: { color: '#F5DEB3' },
+};
+
+export var traceC3 = {
+  x: [20, 30, 40],
+  y: [50, 60, 70],
+  xaxis: 'x13',
+  yaxis: 'y13',
+  type: 'histogram',
+  marker: { color: 'pink' },
+};
+
+export var traceC4 = {
+  x: [20, 30, 40],
+  y: [50, 60, 70],
+  xaxis: 'x14',
+  yaxis: 'y14',
+  type: 'scatter',
+  marker: { color: 'plum' },
+};
+
+export var traceC5 = {
+  x: [20, 30, 40],
+  y: [50, 60, 70],
+  xaxis: 'x15',
+  yaxis: 'y15',
+  type: 'scatter',
+  marker: { color: 'lightblue' },
+};
+
+// row D
+
+export var traceD1 = {
+  x: [1, 2, 3],
+  y: [4, 5, 6],
+  xaxis: 'x16',
+  yaxis: 'y16',    
+  type: 'scatter',
+  marker: { color: '#F5DEB3' },
+};
+
+export var traceD2 = {
+  x: [20, 30, 40],
+  y: [50, 60, 70],
+  xaxis: 'x17',
+  yaxis: 'y17',
+  type: 'scatter',
+  marker: { color: 'pink' },
+};
+
+export var traceD3 = {
+  x: [20, 30, 40],
+  y: [50, 60, 70],
+  xaxis: 'x18',
+  yaxis: 'y18',
+  type: 'scatter',
+  marker: { color: 'plum' },
+};
+
+export var traceD4 = {
+  x: [20, 30, 40],
+  y: [50, 60, 70],
+  xaxis: 'x19',
+  yaxis: 'y19',
+  type: 'histogram',
+  marker: { color: 'lightblue' },
+};
+
+export var traceD5 = {
+  x: [20, 30, 40],
+  y: [50, 60, 70],
+  xaxis: 'x20',
+  yaxis: 'y20',
+  type: 'scatter',
+  marker: { color: 'lightgray' },
+};
+
+// row E
+
+export var traceE1 = {
+  x: [1, 2, 3],
+  y: [4, 5, 6],
+  xaxis: 'x21',
+  yaxis: 'y21',    
+  type: 'scatter',
+  marker: { color: 'pink' },
+};
+
+export var traceE2 = {
+  x: [20, 30, 40],
+  y: [50, 60, 70],
+  xaxis: 'x22',
+  yaxis: 'y22',
+  type: 'scatter',
+  marker: { color: 'plum' },
+};
+
+export var traceE3 = {
+  x: [20, 30, 40],
+  y: [50, 60, 70],
+  xaxis: 'x23',
+  yaxis: 'y23',
+  type: 'scatter',
+  marker: { color: 'lightblue' },
+};
+
+export var traceE4 = {
+  x: [20, 30, 40],
+  y: [50, 60, 70],
+  xaxis: 'x24',
+  yaxis: 'y24',
+  type: 'scatter',
+  marker: { color: 'lightgray' },
+};
+
+export var traceE5 = {
+  x: [20, 30, 40],
+  y: [50, 60, 70],
+  xaxis: 'x25',
+  yaxis: 'y25',
+  type: 'histogram',
+  marker: { color: '#F5DEB3' },
+};
