921fd6b39a59c7b830c2802f440ba8ff219822ee
hayleyskyland <hwitherell@skylandanalytics.net>
2022-01-07 12:56:38 -0700

    Create scatter matrix test

--- app.js
+++ app.js
@@ -3,13 +3,13 @@
 // import { d3MatrixTest } from './charts/tests/d3-matrix/d3-test/d3MatrixTest.js';
 // import { d3Matrix } from './charts/tests/d3-matrix/final/d3Matrix.js';
 // import { plotMatrixTest } from './charts/tests/plot-matrix/plotMatrixTest.js';
-// import { plotMatrixScatterTest } from './charts/tests/plot-matrix/plotMatrixScatterTest.js';
-import { plotMatrix } from './charts/plot-matrix/plotMatrix.js';
+import { plotMatrixScatterTest } from './charts/tests/plot-matrix/plotMatrixScatterTest.js';
+// import { plotMatrix } from './charts/plot-matrix/plotMatrix.js';
 
 // histogram()
 // scatter()
 // d3MatrixTest()
 // d3Matrix()
 // plotMatrixTest()
-// plotMatrixScatterTest()
-plotMatrix()
+plotMatrixScatterTest()
+// plotMatrix()
