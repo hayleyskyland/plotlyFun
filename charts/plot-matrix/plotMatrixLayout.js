605b86588e726c50726be3c742c5834a1dbc42de
hayleyskyland <hwitherell@skylandanalytics.net>
2022-01-07 15:48:55 -0700

    Style borders

--- charts/plot-matrix/plotMatrixLayout.js
+++ charts/plot-matrix/plotMatrixLayout.js
@@ -11,20 +11,12 @@ export const plotMatrixLayout = {
   bargap: 0.05, 
   bargroupgap: 0.2, 
 
-  // shapes: 'black',
-  // plot_bgcolor:"black",
-  // paper_bgcolor:"black",
-  // border: 'solid',
-  // border_width: '1px',
-  // showline: true,
-  
   grid: {
     rows: 5,
     columns: 5,
     pattern: 'independent',
     xgap: 0,
-    ygap: 0,
-    // border: 'solid',
+    ygap: 0
   },
 
   xaxis1: {
@@ -33,559 +25,610 @@ export const plotMatrixLayout = {
     color: 'white',
     ticks: 'outside',
     showline: true,
-    linecolor: 'black',
+    linecolor: 'lightgray',
     mirror: 'ticks',
     ticks: false,
     showticklabels: false
   },
+
   xaxis2: {
     ticklen: 0,
     tickwidth: 0,
     color: 'white',
     ticks: 'outside',
     showline: true,
-    linecolor: 'black',
+    linecolor: 'lightgray',
     mirror: 'ticks',
     ticks: false,
     showticklabels: false
   },
+
   xaxis2: {
     ticklen: 0,
     tickwidth: 0,
     color: 'white',
     ticks: 'outside',
     showline: true,
-    linecolor: 'black',
+    linecolor: 'lightgray',
     mirror: 'ticks',
     ticks: false,
     showticklabels: false
   },
+
   xaxis3: {
     ticklen: 0,
     tickwidth: 0,
     color: 'white',
     ticks: 'outside',
     showline: true,
-    linecolor: 'black',
+    linecolor: 'lightgray',
     mirror: 'ticks',
     ticks: false,
     showticklabels: false
   },
+
   xaxis4: {
     ticklen: 0,
     tickwidth: 0,
     color: 'white',
     ticks: 'outside',
     showline: true,
-    linecolor: 'black',
+    linecolor: 'lightgray',
     mirror: 'ticks',
     ticks: false,
     showticklabels: false
   },
+
   xaxis5: {
     ticklen: 0,
     tickwidth: 0,
     color: 'white',
     ticks: 'outside',
     showline: true,
-    linecolor: 'black',
+    linecolor: 'lightgray',
     mirror: 'ticks',
     ticks: false,
     showticklabels: false
   },
+
   xaxis6: {
     ticklen: 0,
     tickwidth: 0,
     color: 'white',
     ticks: 'outside',
     showline: true,
-    linecolor: 'black',
+    linecolor: 'lightgray',
     mirror: 'ticks',
     ticks: false,
     showticklabels: false
   },
+
   xaxis7: {
     ticklen: 0,
     tickwidth: 0,
     color: 'white',
     ticks: 'outside',
     showline: true,
-    linecolor: 'black',
+    linecolor: 'lightgray',
     mirror: 'ticks',
     ticks: false,
     showticklabels: false
   },
+
   xaxis8: {
     ticklen: 0,
     tickwidth: 0,
     color: 'white',
     ticks: 'outside',
     showline: true,
-    linecolor: 'black',
+    linecolor: 'lightgray',
     mirror: 'ticks',
     ticks: false,
     showticklabels: false
   },
+
   xaxis9: {
     ticklen: 0,
     tickwidth: 0,
     color: 'white',
     ticks: 'outside',
     showline: true,
-    linecolor: 'black',
+    linecolor: 'lightgray',
     mirror: 'ticks',
     ticks: false,
     showticklabels: false
   },
+
   xaxis10: {
     ticklen: 0,
     tickwidth: 0,
     color: 'white',
     ticks: 'outside',
     showline: true,
-    linecolor: 'black',
+    linecolor: 'lightgray',
     mirror: 'ticks',
     ticks: false,
     showticklabels: false
   },
+
   xaxis11: {
     ticklen: 0,
     tickwidth: 0,
     color: 'white',
     ticks: 'outside',
     showline: true,
-    linecolor: 'black',
+    linecolor: 'lightgray',
     mirror: 'ticks',
     ticks: false,
     showticklabels: false
   },
+
   xaxis12: {
     ticklen: 0,
     tickwidth: 0,
     color: 'white',
     ticks: 'outside',
     showline: true,
-    linecolor: 'black',
+    linecolor: 'lightgray',
     mirror: 'ticks',
     ticks: false,
     showticklabels: false
   },
+
   xaxis13: {
     ticklen: 0,
     tickwidth: 0,
     color: 'white',
     ticks: 'outside',
     showline: true,
-    linecolor: 'black',
+    linecolor: 'lightgray',
     mirror: 'ticks',
     ticks: false,
     showticklabels: false
   },
+
   xaxis14: {
     ticklen: 0,
     tickwidth: 0,
     color: 'white',
     ticks: 'outside',
     showline: true,
-    linecolor: 'black',
+    linecolor: 'lightgray',
     mirror: 'ticks',
     ticks: false,
     showticklabels: false
   },
+
   xaxis15: {
     ticklen: 0,
     tickwidth: 0,
     color: 'white',
     ticks: 'outside',
     showline: true,
-    linecolor: 'black',
+    linecolor: 'lightgray',
     mirror: 'ticks',
     ticks: false,
     showticklabels: false
   },
+
   xaxis16: {
     ticklen: 0,
     tickwidth: 0,
     color: 'white',
     ticks: 'outside',
     showline: true,
-    linecolor: 'black',
+    linecolor: 'lightgray',
     mirror: 'ticks',
     ticks: false,
     showticklabels: false
   },
+
   xaxis17: {
     ticklen: 0,
     tickwidth: 0,
     color: 'white',
     ticks: 'outside',
     showline: true,
-    linecolor: 'black',
+    linecolor: 'lightgray',
     mirror: 'ticks',
     ticks: false,
     showticklabels: false
   },
+
   xaxis18: {
     ticklen: 0,
     tickwidth: 0,
     color: 'white',
     ticks: 'outside',
     showline: true,
-    linecolor: 'black',
+    linecolor: 'lightgray',
     mirror: 'ticks',
     ticks: false,
     showticklabels: false
   },
+
   xaxis19: {
     ticklen: 0,
     tickwidth: 0,
     color: 'white',
     ticks: 'outside',
     showline: true,
-    linecolor: 'black',
+    linecolor: 'lightgray',
     mirror: 'ticks',
     ticks: false,
     showticklabels: false
   },
+
   xaxis20: {
     ticklen: 0,
     tickwidth: 0,
     color: 'white',
     ticks: 'outside',
     showline: true,
-    linecolor: 'black',
+    linecolor: 'lightgray',
     mirror: 'ticks',
     ticks: false,
     showticklabels: false
   },
+
   xaxis21: {
     ticklen: 0,
     tickwidth: 0,
     color: 'white',
     ticks: 'outside',
     showline: true,
-    linecolor: 'black',
+    linecolor: 'lightgray',
     mirror: 'ticks',
     ticks: false,
     showticklabels: false
   },
+
   xaxis22: {
     ticklen: 0,
     tickwidth: 0,
     color: 'white',
     ticks: 'outside',
     showline: true,
-    linecolor: 'black',
+    linecolor: 'lightgray',
     mirror: 'ticks',
     ticks: false,
     showticklabels: false
   },
+
   xaxis23: {
     ticklen: 0,
     tickwidth: 0,
     color: 'white',
     ticks: 'outside',
     showline: true,
-    linecolor: 'black',
+    linecolor: 'lightgray',
     mirror: 'ticks',
     ticks: false,
     showticklabels: false
   },
+
   xaxis24: {
     ticklen: 0,
     tickwidth: 0,
     color: 'white',
     ticks: 'outside',
     showline: true,
-    linecolor: 'black',
+    linecolor: 'lightgray',
     mirror: 'ticks',
     ticks: false,
     showticklabels: false
   },
+
   xaxis25: {
     ticklen: 0,
     tickwidth: 0,
     color: 'white',
     ticks: 'outside',
     showline: true,
-    linecolor: 'black',
+    linecolor: 'lightgray',
     mirror: 'ticks',
     ticks: false,
     showticklabels: false
   },
+
   yaxis1: {
     ticklen: 0,
     tickwidth: 0,
     color: 'white',
     ticks: 'outside',
     showline: true,
-    linecolor: 'black',
+    linecolor: 'lightgray',
     mirror: 'ticks',
     ticks: false,
     showticklabels: false
   },
+
   yaxis2: {
     ticklen: 0,
     tickwidth: 0,
     ticks: 'outside',
-    linecolor: 'black',
+    linecolor: 'lightgray',
     ticks: false,
     showline: true,
     color: 'white',
     mirror: 'ticks',
     showticklabels: false
   },
+
   yaxis3: {
     ticklen: 0,
     tickwidth: 0,
     color: 'white',
     ticks: 'outside',
     showline: true,
-    linecolor: 'black',
+    linecolor: 'lightgray',
     mirror: 'ticks',
     ticks: false,
     showticklabels: false
   },
+
   yaxis4: {
     ticklen: 0,
     tickwidth: 0,
     color: 'white',
     ticks: 'outside',
     showline: true,
-    linecolor: 'black',
+    linecolor: 'lightgray',
     mirror: 'ticks',
     ticks: false,
     showticklabels: false
   },
+
   yaxis5: {
     ticklen: 0,
     tickwidth: 0,
     color: 'white',
     ticks: 'outside',
     showline: true,
-    linecolor: 'black',
+    linecolor: 'lightgray',
     mirror: 'ticks',
     ticks: false,
     showticklabels: false
   },
+
   yaxis6: {
     ticklen: 0,
     tickwidth: 0,
     color: 'white',
     ticks: 'outside',
     showline: true,
-    linecolor: 'black',
+    linecolor: 'lightgray',
     mirror: 'ticks',
     ticks: false,
     showticklabels: false
   },
+
   yaxis7: {
     ticklen: 0,
     tickwidth: 0,
     color: 'white',
     ticks: 'outside',
     showline: true,
-    linecolor: 'black',
+    linecolor: 'lightgray',
     mirror: 'ticks',
     ticks: false,
     showticklabels: false
   },
+
   yaxis8: {
     ticklen: 0,
     tickwidth: 0,
     color: 'white',
     ticks: 'outside',
     showline: true,
-    linecolor: 'black',
+    linecolor: 'lightgray',
     mirror: 'ticks',
     ticks: false,
     showticklabels: false
   },
+
   yaxis9: {
     ticklen: 0,
     tickwidth: 0,
     color: 'white',
     ticks: 'outside',
     showline: true,
-    linecolor: 'black',
+    linecolor: 'lightgray',
     mirror: 'ticks',
     ticks: false,
     showticklabels: false
   },
+
   yaxis10: {
     ticklen: 0,
     tickwidth: 0,
     color: 'white',
     ticks: 'outside',
     showline: true,
-    linecolor: 'black',
+    linecolor: 'lightgray',
     mirror: 'ticks',
     ticks: false,
     showticklabels: false
   },
+
   yaxis11: {
     ticklen: 0,
     tickwidth: 0,
     color: 'white',
     ticks: 'outside',
     showline: true,
-    linecolor: 'black',
+    linecolor: 'lightgray',
     mirror: 'ticks',
     ticks: false,
     showticklabels: false
   },
+
   yaxis12: {
     ticklen: 0,
     tickwidth: 0,
     color: 'white',
     ticks: 'outside',
     showline: true,
-    linecolor: 'black',
+    linecolor: 'lightgray',
     mirror: 'ticks',
     ticks: false,
     showticklabels: false
   },
+
   yaxis13: {
     ticklen: 0,
     tickwidth: 0,
     color: 'white',
     ticks: 'outside',
     showline: true,
-    linecolor: 'black',
+    linecolor: 'lightgray',
     mirror: 'ticks',
     ticks: false,
     showticklabels: false
   },
+
   yaxis14: {
     ticklen: 0,
     tickwidth: 0,
     color: 'white',
     ticks: 'outside',
     showline: true,
-    linecolor: 'black',
+    linecolor: 'lightgray',
     mirror: 'ticks',
     ticks: false,
     showticklabels: false
   },
+
   yaxis15: {
     ticklen: 0,
     tickwidth: 0,
     color: 'white',
     ticks: 'outside',
     showline: true,
-    linecolor: 'black',
+    linecolor: 'lightgray',
     mirror: 'ticks',
     ticks: false,
     showticklabels: false
   },
+
   yaxis16: {
     ticklen: 0,
     tickwidth: 0,
     color: 'white',
     ticks: 'outside',
     showline: true,
-    linecolor: 'black',
+    linecolor: 'lightgray',
     mirror: 'ticks',
     ticks: false,
     showticklabels: false
   },
+
   yaxis17: {
     ticklen: 0,
     tickwidth: 0,
     color: 'white',
     ticks: 'outside',
     showline: true,
-    linecolor: 'black',
+    linecolor: 'lightgray',
     mirror: 'ticks',
     ticks: false,
     showticklabels: false
   },
+
   yaxis18: {
     ticklen: 0,
     tickwidth: 0,
     color: 'white',
     ticks: 'outside',
     showline: true,
-    linecolor: 'black',
+    linecolor: 'lightgray',
     mirror: 'ticks',
     ticks: false,
     showticklabels: false
   },
+
   yaxis19: {
     ticklen: 0,
     tickwidth: 0,
     color: 'white',
     ticks: 'outside',
     showline: true,
-    linecolor: 'black',
+    linecolor: 'lightgray',
     mirror: 'ticks',
     ticks: false,
     showticklabels: false
   },
+
   yaxis20: {
     ticklen: 0,
     tickwidth: 0,
     color: 'white',
     ticks: 'outside',
     showline: true,
-    linecolor: 'black',
+    linecolor: 'lightgray',
     mirror: 'ticks',
     ticks: false,
     showticklabels: false
   },
+
   yaxis21: {
     ticklen: 0,
     tickwidth: 0,
     color: 'white',
     ticks: 'outside',
     showline: true,
-    linecolor: 'black',
+    linecolor: 'lightgray',
     mirror: 'ticks',
     ticks: false,
     showticklabels: false
   },
+
   yaxis22: {
     ticklen: 0,
     tickwidth: 0,
     color: 'white',
     ticks: 'outside',
     showline: true,
-    linecolor: 'black',
+    linecolor: 'lightgray',
     mirror: 'ticks',
     ticks: false,
     showticklabels: false
   },
+
   yaxis23: {
     ticklen: 0,
     tickwidth: 0,
     color: 'white',
     ticks: 'outside',
     showline: true,
-    linecolor: 'black',
+    linecolor: 'lightgray',
     mirror: 'ticks',
     ticks: false,
     showticklabels: false
   },
+
   yaxis24: {
     ticklen: 0,
     tickwidth: 0,
     color: 'white',
     ticks: 'outside',
     showline: true,
-    linecolor: 'black',
+    linecolor: 'lightgray',
     mirror: 'ticks',
     ticks: false,
     showticklabels: false
   },
+
   yaxis25: {
     ticklen: 0,
     tickwidth: 0,
     color: 'white',
     ticks: 'outside',
     showline: true,
-    linecolor: 'black',
+    linecolor: 'lightgray',
     mirror: 'ticks',
     ticks: false,
     showticklabels: false
   },
+
 };
